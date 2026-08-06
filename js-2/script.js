26747f82920c6eb7e7afb71678b4d110517534f4
Ibrahim <siddiqueibrahim77@gmail.com>
2026-08-06 06:50:30 -0700

    Operators, Comparisons & Conditional Logic

---
+++ js-2/script.js
@@ -0,0 +1,24 @@
+
+let username = prompt("Enter your username: ");
+let password = prompt("Enter your password: ");
+
+if (username === "" || username === null || password === "" || password === null) {
+    alert("Please enter both username and password.");
+} else {
+
+    if (username === "admin" && password === "12345") {
+
+        alert("Welcome Admin!");
+        console.log("Logged in as admin");
+
+    } else if (username === "admin" && password !== "12345") {
+
+        alert("Incorrect password. Please try again.");
+        console.log("Access denied")
+
+    } else if (username !== "admin") {
+
+        alert("User not found");
+        console.log("Access denied");
+    }
+}
